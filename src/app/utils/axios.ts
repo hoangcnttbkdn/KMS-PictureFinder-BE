4112713885708e259684dd2cfd6c9ec53411c143
VoVanThanh <thanhvo618@gmail.com>
2022-11-13 23:43:42 +0700

    feat: kms 21 setup axios call api

---
+++ src/app/utils/axios.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export const createAxios = (axiosConfig: {
+  baseUrl: string
+  headers: object
+}) => {
+  const axiosClient = axios.create({
+    baseURL: axiosConfig.baseUrl,
+    headers: { ...axiosConfig.headers },
+  })
+
+  axiosClient.interceptors.request.use(async (config) => {
+    return config
+  })
+
+  axiosClient.interceptors.response.use(
+    (response) => {
+      if (response && response.data) {
+        return response.data
+      }
+      return response
+    },
+    async (error) => {
+      throw error
+    },
+  )
+
+  return axiosClient
+}
