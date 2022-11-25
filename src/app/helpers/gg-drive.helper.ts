191c76fa856fb1225646f86db5b08aa48c56b5fe
phuocleoceo <phuoc.truong@emandai.net>
2022-11-25 22:51:45 +0700

    [GG-Drive] enhance gg limit

--- src/app/helpers/gg-drive.helper.ts
+++ src/app/helpers/gg-drive.helper.ts
@@ -1,117 +1,127 @@
 /* eslint-disable @typescript-eslint/ban-types */
+import { JSONClient } from 'google-auth-library/build/src/auth/googleauth'
 import { GetFileList } from 'google-drive-getfilelist'
-import { logger } from '../../shared/providers'
+import { GoogleOAuthHelper } from './gg-oauth.helper'
+import { drive_v3, google } from 'googleapis'
 import { Readable } from 'stream'
 import FormData from 'form-data'
 import axios from 'axios'
 
 export class GoogleDriveHelper {
-  private GG_API_KEY: string = process.env.GG_API_KEY
+  private googleOAuthHelper: GoogleOAuthHelper
   private AI_SERVER_URL = `${process.env.AI_API_SERVER}/face-findor`
 
-  public getGoogleImgLink = (folderId: string, callback: Function) => {
-    const resource = {
-      auth: this.GG_API_KEY,
-      id: folderId,
-      fields: 'files(id,name)',
-    }
-
-    return GetFileList(resource, (err: any, res: any) => {
-      if (err) {
-        logger.error(err)
-        callback([])
-        return
-      }
-      const files: FileDriveResponse[] = res.fileList.flatMap(
-        ({ files }) => files,
-      )
-      const fileInfors: FileInfor[] = files.map(
-        (f: FileDriveResponse): FileInfor => {
-          return {
-            id: f.id,
-            name: f.name,
-            url: `https://www.googleapis.com/drive/v3/files/${f.id}?alt=media&key=${this.GG_API_KEY}`,
-          }
-        },
-      )
-      callback(fileInfors)
-    })
+  constructor() {
+    this.googleOAuthHelper = new GoogleOAuthHelper()
   }
 
-  public recognizeWithGGDrive = (
+  public recognizeWithGGDrive = async (
     folderUrl: string,
     targetImage: Buffer,
-    callback: Function,
-  ) => {
-    const folderId = getGGDriveFolderId(folderUrl)
+  ): Promise<FileInfor[]> => {
+    const folderId = this.getGGDriveFolderId(folderUrl)
     if (!folderId) {
-      callback(null, { message: 'This folderUrl is not contain folderId' })
-      return
+      throw { message: 'This folderUrl is not contain folderId' }
     }
 
+    const auth = await this.googleOAuthHelper.authorize()
+    const drive = google.drive({
+      auth,
+      version: 'v3',
+    })
+
     const formData = new FormData()
-    this.getGoogleImgLink(folderId, async (files: FileInfor[]) => {
-      // LIST IMAGES
-      await Promise.all(
-        files.map(async (file: FileInfor) => {
-          const fileRes = await axios({
-            url: file.url,
-            responseType: 'arraybuffer',
-          })
-          const buffer64 = Buffer.from(fileRes.data, 'binary')
-          formData.append('list_images', Readable.from(buffer64), file.name)
-        }),
-      )
-      // TARGET IMAGE
-      formData.append('target_image', Readable.from(targetImage), 'target.png')
+    const files: FileInfor[] = await this.getGoogleImgLink(folderId, auth)
 
-      const config = {
-        method: 'post',
-        url: this.AI_SERVER_URL,
-        headers: { ...formData.getHeaders() },
-        data: formData,
+    // LIST IMAGES
+    await Promise.all(
+      files.map(async (file) => {
+        const buffer64: any = await this.getGGBuffer(drive, file)
+        formData.append('list_images', Readable.from(buffer64), file.id)
+      }),
+    )
+    // TARGET IMAGE
+    formData.append('target_image', Readable.from(targetImage), 'target.png')
+
+    const config = {
+      method: 'post',
+      url: this.AI_SERVER_URL,
+      headers: { ...formData.getHeaders() },
+      data: formData,
+    }
+
+    try {
+      const { data: apiData } = await axios(config)
+      const result: FileInfor[] = []
+      Object.keys(apiData).forEach((key: string) => {
+        const value = apiData[key]
+        if (value['match_face']) {
+          const matchFile = files.find((item) => key.includes(item.id))
+          result.push(matchFile)
+        }
+      })
+      return result
+    } catch (err) {
+      throw { message: 'Error when call AI server' }
+    }
+  }
+
+  private getGGDriveFolderId = (folderUrl: string): string => {
+    const regex = /(?<=folders\/)[^? \n\r\t]*/
+    const expression = folderUrl.match(regex)
+    if (expression != null) {
+      return expression[0]
+    }
+    return ''
+  }
+
+  private getGoogleImgLink = (
+    folderId: string,
+    auth: JSONClient,
+  ): Promise<FileInfor[]> => {
+    return new Promise((resolve, reject) => {
+      const resource = {
+        auth,
+        id: folderId,
+        fields: 'files(id,name,thumbnailLink)',
       }
-
-      try {
-        const { data: apiData } = await axios(config)
-        const result = []
-        Object.keys(apiData).forEach((key: string) => {
-          const value = apiData[key]
-          if (value['match_face']) {
-            const matchFile = files.find((item) => key.includes(item.name))
-            // result.push({
-            //   ...value,
-            //   ...matchFile,
-            // })
-            result.push({
-              id: matchFile.id,
-              url: matchFile.url,
-            })
+      GetFileList(resource, (err: any, res: any) => {
+        if (err) return reject([])
+        const files = res.fileList.flatMap(({ files }) => files)
+        const fileInfors: FileInfor[] = files.map((f: any): FileInfor => {
+          return {
+            id: f.id || '',
+            url: f.thumbnailLink || '',
           }
         })
-        callback(result, null)
-      } catch (err) {
-        callback(null, err)
-      }
+        resolve(fileInfors)
+      })
     })
   }
-}
 
-const getGGDriveFolderId = (folderUrl: string) => {
-  const regex = /(?<=folders\/)[^? \n\r\t]*/
-  const expression = folderUrl.match(regex)
-  if (expression != null) {
-    return expression[0]
+  private getGGBuffer = (
+    drive: drive_v3.Drive,
+    file: FileInfor,
+  ): Promise<Buffer> => {
+    return new Promise((resolve, reject) => {
+      drive.files.get(
+        {
+          fileId: file.id,
+          alt: 'media',
+        },
+        {
+          responseType: 'arraybuffer',
+        },
+        (err: any, res: any) => {
+          if (err) return reject(null)
+          resolve(Buffer.from(res.data, 'binary'))
+        },
+      )
+    })
   }
-  return ''
-}
-interface FileDriveResponse {
-  id: string
-  name: string
 }
 
 interface FileInfor {
   id: string
-  name: string
   url: string
 }
